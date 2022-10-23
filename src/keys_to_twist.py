b5750d6e09ef002a95874dae76cb2a947e847a02
aboelela924 <aboelela924x10@gmail.com>
2022-10-23 11:10:10 +0200

    add parametrization to the speed

--- src/keys_to_twist.py
+++ src/keys_to_twist.py
@@ -15,13 +15,14 @@ mapping = {
 }
 
 g_last_twist = None
+g_sclaing_factors = [1.0, 1.0]
 
 def callback(msg, pub):
-    global g_last_twist
+    global g_last_twist, g_sclaing_factors
     if(msg.data in mapping):
         speed = mapping[msg.data]
-        g_last_twist.linear.x = speed[0]
-        g_last_twist.angular.z = speed[1]
+        g_last_twist.linear.x = speed[0] * g_sclaing_factors[0]
+        g_last_twist.angular.z = speed[1] * g_sclaing_factors[1]
         pub.publish(g_last_twist)
         
 
@@ -33,6 +34,13 @@ if __name__ == "__main__":
     # rospy.spin()    
     rate = rospy.Rate(10)
     g_last_twist = Twist()
+    
+    if rospy.has_param('~linear_scale'):
+        g_sclaing_factors[0] = rospy.get_param('~linear_scale')
+        
+    if rospy.has_param('~angular_scale'):
+        g_sclaing_factors[1] = rospy.get_param('~angular_scale')
+    
     while not rospy.is_shutdown():
         pub.publish(g_last_twist)
         rate.sleep()
